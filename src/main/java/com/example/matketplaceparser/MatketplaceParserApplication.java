f8f2583ca855f12bd90cd5f7e4f5bbc0a83d3e1b
Timofey <oxxxymofey@gmail.com>
2025-05-03 00:16:41 +0300

    First

---
+++ src/main/java/com/example/matketplaceparser/MatketplaceParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.matketplaceparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatketplaceParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatketplaceParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/matketplaceparser/MatketplaceParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matketplaceparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatketplaceParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
